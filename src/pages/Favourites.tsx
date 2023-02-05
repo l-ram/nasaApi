67a1c5f6d0f59a31eee972c59d9e5a89cd59527c
l-ram <larryramjutton@gmail.com>
2023-02-05 14:56:24 +0100

    wip

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -24,95 +24,99 @@ export default function FavouritesPage() {
     }
     )();
 
+    const removeFromLocalStorage = (itemUrl: string) => {
+        //ts-ignore
+        if (itemUrl) {
+// Remove element from favourites OBJECT, then update the localstorage with the new Object
+            favourites = JSON.parse(localStorage.getItem('nasaFavourites') || "{}");
+            localStorage.removeItem('nasaFavourites');
+        }
+    }
     // Converting object to array
     let arrayOfFavourites: IFavouritesArray[] = Object.values(favourites);
 
     const style = {
-        position: 'absolute' as 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: '#fff',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-    };
+            position: 'absolute' as 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: '#fff',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+        };
 
-    // Modal
-    const [open, setOpen] = useState<boolean>(false);
-    const [modalData, setModalData] = useState<string>("");
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+        // Modal
+        const [open, setOpen] = useState<boolean>(false);
+        const [modalData, setModalData] = useState<string>("");
+        const handleOpen = () => setOpen(true);
+        const handleClose = () => setOpen(false);
 
-    // Open Image OnClick function
-    const openInNewTab = (url: string) => {
-        window.open(url)
-    }
+        // Open Image OnClick function
+        const openInNewTab = (url: string) => {
+            window.open(url)
+        }
 
-    // Remove from favourites LocalStorage
+        return (
+            <div>
 
-    const removeFromLocalStorage = () => {
-
-    }
-
-    return (
-        <div>
-
-            <Grid container
-                padding={5}
-                alignContent={"center"}
-                spacing={3}
-            >
-                {arrayOfFavourites.map((nasa, key: number) => (
-                    <Grid item xs={3} key={key}>
-                        <Card sx={{ width: '100%', maxWidth: 800, margin: 'auto' }}>
-                            <CardMedia
-                                component="img"
-                                src={nasa.url}
-                                onClick={() => { openInNewTab(nasa.hdurl) }}
-                            >
-                            </CardMedia>
-                            <CardContent>
-                                <Typography>{nasa.title}</Typography>
-                                <Typography>{`${nasa.explanation.substring(0, 50)}...`}</Typography>
-                                <Button onClick={() => {
-                                    setModalData(nasa.explanation);
-                                    handleOpen();
-                                }}>
-                                    Expand explanation
-                                </Button>
-
-                                <Typography>{nasa.date}</Typography>
-                                <Typography>{nasa.copyright}</Typography>
-                                <Button color="error" onClick={removeFromLocalStorage}>
-                                    Remove from favourites
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
-                <Modal
-                    aria-labelledby="transition-modal-title"
-                    aria-describedby="transition-modal-description"
-                    open={open}
-                    onClose={handleClose}
-                    closeAfterTransition
+                <Grid container
+                    padding={5}
+                    alignContent={"center"}
+                    spacing={3}
                 >
-                    <Fade in={open}>
-                        <Box sx={style}>
-                            <Typography id="transition-modal-title" variant="h6" component="h2">
-                                Explanation
-                            </Typography>
-                            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                                {modalData}
-                            </Typography>
-                        </Box>
-                    </Fade>
-                </Modal>
-            </Grid>
+                    {arrayOfFavourites.map((nasa, key: number) => (
+                        <Grid item xs={3} key={key}>
+                            <Card sx={{ width: '100%', maxWidth: 800, margin: 'auto' }}>
+                                <CardMedia
+                                    component="img"
+                                    src={nasa.url}
+                                    onClick={() => { openInNewTab(nasa.hdurl) }}
+                                >
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography>{nasa.title}</Typography>
+                                    <Typography>{`${nasa.explanation.substring(0, 50)}...`}</Typography>
+                                    <Button onClick={() => {
+                                        setModalData(nasa.explanation);
+                                        handleOpen();
+                                    }}>
+                                        Expand explanation
+                                    </Button>
 
-        </div>
+                                    <Typography>{nasa.date}</Typography>
+                                    <Typography>{nasa.copyright}</Typography>
+                                    <Button color="error" data-button-key={nasa.url} onClick={(e) => {
+                                        removeFromLocalStorage(e.currentTarget.getAttribute("data-button-key")!);
+                                    }}>
+                                        Remove from favourites
+                                    </Button>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                    <Modal
+                        aria-labelledby="transition-modal-title"
+                        aria-describedby="transition-modal-description"
+                        open={open}
+                        onClose={handleClose}
+                        closeAfterTransition
+                    >
+                        <Fade in={open}>
+                            <Box sx={style}>
+                                <Typography id="transition-modal-title" variant="h6" component="h2">
+                                    Explanation
+                                </Typography>
+                                <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                                    {modalData}
+                                </Typography>
+                            </Box>
+                        </Fade>
+                    </Modal>
+                </Grid>
 
-    )
-}
+            </div>
+
+        )
+    }
