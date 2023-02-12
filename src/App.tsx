225c3cab7f8167a98c84e4a6f499efd1d8a1d4f4
l-ram <larryramjutton@gmail.com>
2023-02-12 02:05:39 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import './App.css';
 import { Gallery } from './components/Gallery';
 import { ThemeProvider, createTheme, AppBar, Menu, Typography, IconButton, Toolbar, Avatar, Box, Button, MenuItem, Tooltip } from '@mui/material';
-import { Rocket, Reorder } from '@mui/icons-material'
+import { Rocket, Reorder, Brightness7Rounded, Brightness4Rounded } from '@mui/icons-material'
 import { Sidebar } from './components/Sidebar';
-import { useState } from 'react';
+import { createContext, useContext, useMemo, useState } from 'react';
 import Favourites from './pages/Favourites';
 import { Route, Routes } from 'react-router-dom';
 
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  }
-})
+
+const ColorModeContext = createContext({ toggleColorMode: () => { } });
+
+function ColourMode() {
+  const theme = createTheme();
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'text.primary',
+        pr: 5
+      }}
+    >
+      <Tooltip title={"Toggle Light/Dark mode"}>
+        <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === 'dark' ? <Brightness7Rounded /> : <Brightness4Rounded />}
+        </IconButton>
+      </Tooltip>
+    </Box>
+
+  );
+};
 
 function App() {
 
+  // UI
   const [openDraw, setOpenDraw] = useState<boolean>(false)
 
   const handleDrawerOpen = () => {
@@ -34,70 +56,99 @@ function App() {
     setAnchorElUser(null);
   };
 
+  // Dark or Light theme
+
+  const [mode, setMode] = useState<'light' | 'dark'>('dark');
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
   return (
     <div className="App">
       <header className="App-header">
 
-        <ThemeProvider theme={theme}>
-          <AppBar position='fixed'>
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
-              // sx={{ mr: 2, ...(openDraw && { display: 'none' }) }}
-              >
-                <Reorder />
-              </IconButton>
-              <Rocket sx={{ display: { xs: 'none', sm: 'block' }, mr: 1 }} />
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="/"
-                sx={{
-                  mr: 2,
-                  display: { xs: 'none', md: 'flex' },
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  letterSpacing: '.3rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-              >
-                NASA
-              </Typography>
-              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider theme={theme}>
+            <AppBar position='fixed'>
+              <Toolbar>
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={handleDrawerOpen}
+                  edge="start"
+                >
+                  <Reorder />
+                </IconButton>
+                <Rocket color='primary' sx={{ display: { sm: 'block' }, mr: 1 }} />
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="a"
+                  href="/"
+                  sx={{
+                    mr: 2,
+                    display: { md: 'flex' },
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                  }}
+                >
+                  NASA
+                </Typography>
+                <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
                   <Button
                     sx={{ my: 2, color: 'white', display: 'block' }}
                   >
                     ASTRONOMY PHOTO OF THE DAY
                   </Button>
-              </Box>
+                </Box>
 
-              <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Nele" src="/static/images/avatar/1.jpg" />
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: '45px' }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
+
+                <Box sx={{ display: { xs: 'none', sm:'flex' }}}>
+
+                  <ColourMode></ColourMode>
+                </Box>
+
+
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Avatar alt="Nele" src="/static/images/avatar/1.jpg" />
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    sx={{ mt: '45px' }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                  >
                     <MenuItem onClick={handleCloseUserMenu}>
                       <Typography textAlign="center">Profile</Typography>
                     </MenuItem>
@@ -107,20 +158,22 @@ function App() {
                     <MenuItem onClick={handleCloseUserMenu}>
                       <Typography textAlign="center">Logout</Typography>
                     </MenuItem>
-                </Menu>
-              </Box>
-            </Toolbar>
-          </AppBar>
-          <Sidebar
-            openDraw={openDraw}
-            handleDrawerClose={handleDrawerClose}
-          />
-          <Routes>
-            <Route path="/" element={<Gallery />} />
-            <Route path="favourites" element={<Favourites />} />
-          </Routes>
-        </ThemeProvider>
+                  </Menu>
+                </Box>
 
+
+              </Toolbar>
+            </AppBar>
+            <Sidebar
+              openDraw={openDraw}
+              handleDrawerClose={handleDrawerClose}
+            />
+            <Routes>
+              <Route path="/" element={<Gallery />} />
+              <Route path="favourites" element={<Favourites />} />
+            </Routes>
+          </ThemeProvider>
+        </ColorModeContext.Provider>
       </header>
     </div>
   );
--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,11 +1,23 @@
 
-import { Box, Button, Card, CardContent, CardMedia, Fade, Grid, Modal, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardMedia, Fade, Grid, Modal, TextField, Typography } from "@mui/material";
 import { Cancel } from '@mui/icons-material'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IFavouritesObject, IFavouritesArray } from "../types";
 
 
 export default function FavouritesPage() {
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: '#fff',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
 
     let favourites: IFavouritesObject = {};
 
@@ -16,32 +28,18 @@ export default function FavouritesPage() {
     }
     )();
 
-    const removeFavourite = (url:string) => {
+    const removeFavourite = (url: string) => {
         if (favourites[url]) {
             delete favourites[url];
             localStorage.setItem('nasaFavourites', JSON.stringify(favourites));
         };
     };
 
-    // Converting object to array
-
-    const style = {
-            position: 'absolute' as 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: 400,
-            bgcolor: '#fff',
-            border: '2px solid #000',
-            boxShadow: 24,
-            p: 4,
-        };
-
-        // Modal
-        const [open, setOpen] = useState<boolean>(false);
-        const [modalData, setModalData] = useState<string>("");
-        const handleOpen = () => setOpen(true);
-        const handleClose = () => setOpen(false);
+    // Modal
+    const [open, setOpen] = useState<boolean>(false);
+    const [modalData, setModalData] = useState<string>("");
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     // Open Image OnClick function
     const openInNewTab = (url: string) => {
@@ -49,37 +47,64 @@ export default function FavouritesPage() {
     }
 
     // Converting object to array
-
-
     let arrayOfFavourites: IFavouritesArray[] = Object.values(favourites);
 
-        return (
-            <div>
+    // Searchbar for filtering
+    const [searchText, setSearchText] = useState<string>('');
+    const [filteredFaves, setFilteredFaves] = useState(arrayOfFavourites);
+    console.log(searchText);
+    const onSearchChange = (search: string) => {
+        setSearchText(search);
+    }
+    useEffect(() => {
+        const newfilteredFavourites = arrayOfFavourites.filter((faveFilter) => {
+            return faveFilter.title.toLowerCase().includes(searchText);
+        });
 
-                <Grid container
-                    padding={6}
-                    marginTop={5}
-                    alignContent={"center"}
-                    spacing={3}
-                >
-                    {arrayOfFavourites.map((nasa, key: number) => (
-                        <Grid item xs={12} sm={6} md={4} key={key}>
-                            <Card sx={{ width: '100%', maxWidth: 800, margin: 'auto' }}>
-                                <CardMedia
-                                    component="img"
-                                    src={nasa.url}
-                                    onClick={() => { openInNewTab(nasa.hdurl) }}
-                                >
-                                </CardMedia>
-                                <CardContent>
-                                    <Typography>{nasa.title}</Typography>
-                                    <Typography>{`${nasa.explanation.substring(0, 50)}...`}</Typography>
-                                    <Button onClick={() => {
-                                        setModalData(nasa.explanation);
-                                        handleOpen();
-                                    }}>
-                                        Expand explanation
-                                    </Button>
+        setFilteredFaves(newfilteredFavourites);
+
+    }, [searchText]);
+
+    return (
+        <div>
+
+            <Box sx={{
+                '& > :not(style)': { mt: 15, width: '25ch' },
+            }}>
+                <TextField
+
+                    id="filled-basic"
+                    label="Filter"
+                    variant="filled"
+                    onChange={(e) => { onSearchChange(e.target.value) }}
+
+                />
+            </Box>
+
+            <Grid container
+                padding={6}
+                marginTop={5}
+                alignContent={"center"}
+                spacing={3}
+            >
+                {filteredFaves.map((nasa, key: number) => (
+                    <Grid item xs={12} sm={6} md={4} key={key}>
+                        <Card sx={{ width: '100%', maxWidth: 800, margin: 'auto' }}>
+                            <CardMedia
+                                component="img"
+                                src={nasa.url}
+                                onClick={() => { openInNewTab(nasa.hdurl) }}
+                            >
+                            </CardMedia>
+                            <CardContent>
+                                <Typography>{nasa.title}</Typography>
+                                <Typography>{`${nasa.explanation.substring(0, 50)}...`}</Typography>
+                                <Button onClick={() => {
+                                    setModalData(nasa.explanation);
+                                    handleOpen();
+                                }}>
+                                    Expand explanation
+                                </Button>
 
                                 <Typography>{nasa.date}</Typography>
                                 <Typography>{nasa.copyright}</Typography>
@@ -112,7 +137,9 @@ export default function FavouritesPage() {
                 </Modal>
             </Grid>
 
-            </div>
 
-        )
-    }
+
+        </div>
+
+    )
+}
